3aa466a5b2c876261f67e88c5a3e7cdd8138fc8b
Bejirrr <animelive24jam@gmail.com>
2025-12-31 14:27:45 +0700

    Delete Voltra.lua

--- Voltra.lua
+++
@@ -1,6 +0,0 @@
-
--- If you are seeing this, it means your executor is not supported by Luarmor.
--- Create a ticket in gg/luarmor and provide details.
-
-game:GetService("Players").LocalPlayer:Kick("Your executor is not supported by Luarmor. Create a ticket in .gg/luarmor and provide us the details so we can support it.")
-while wait(1) do end;
